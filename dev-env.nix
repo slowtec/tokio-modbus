3782c775bc41904327446cca39d2577da5e0ddab
Markus Kohlhase <markus.kohlhase@slowtec.de>
2018-12-07 16:04:59 +0100

    use stable rust in dev-env.nix

--- dev-env.nix
+++ dev-env.nix
@@ -1,7 +1,7 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> { overlays = [ moz_overlay ]; };
-  rustChannel = pkgs.rustChannelOf { channel = "beta"; };
+  rustChannel = pkgs.rustChannelOf { channel = "stable"; };
 in
   with pkgs;
   stdenv.mkDerivation {
