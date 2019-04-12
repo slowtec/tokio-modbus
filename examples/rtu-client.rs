56727bdc2ac359c58bbd4e74f6fa62d96a87f2c5
Uwe Klotz <uwe.klotz@slowtec.de>
2019-04-12 11:35:02 +0200

    Use SharedContext in rtu-client example

--- examples/rtu-client.rs
+++ examples/rtu-client.rs
@@ -1,47 +1,94 @@
-use tokio_modbus::prelude::*;
+use futures::{future, Future};
+
+use std::{cell::RefCell, io::Error, rc::Rc};
+
+use tokio_core::reactor::{Core, Handle};
+
+use tokio_modbus::prelude::{
+    client::util::{reconnect_shared_context, NewContext, SharedContext},
+    *,
+};
 
 const SLAVE_1: Slave = Slave(0x01);
 const SLAVE_2: Slave = Slave(0x02);
 
 #[cfg(feature = "rtu")]
 pub fn main() {
-    use futures::Future;
-    use tokio_core::reactor::Core;
     use tokio_serial::{Serial, SerialPortSettings};
 
     let mut core = Core::new().unwrap();
-    let handle = core.handle();
-    let tty_path = "/dev/ttyUSB0";
 
-    let mut settings = SerialPortSettings::default();
-    settings.baud_rate = 19200;
-    let port = Serial::from_path_with_handle(tty_path, &settings, &handle.new_tokio_handle())
-        .expect(&format!("Unable to open serial device '{}'", tty_path));
+    #[derive(Debug)]
+    struct SerialConfig {
+        path: String,
+        settings: SerialPortSettings,
+        handle: Handle,
+    }
 
-    // On Unix you might disable the `exclusive` flag:
-    // port.set_exclusive(false)
-    //     .expect("Unable to set serial port exlusive");
+    impl NewContext for SerialConfig {
+        fn new_context(&self) -> Box<dyn Future<Item = client::Context, Error = Error>> {
+            let handle = self.handle.clone();
+            Box::new(
+                future::result(Serial::from_path_with_handle(
+                    &self.path,
+                    &self.settings,
+                    &self.handle.new_tokio_handle(),
+                ))
+                .and_then(move |port| rtu::connect(&handle, port)),
+            )
+        }
+    }
 
-    let task = rtu::connect(&handle, port)
-        .and_then(move |mut ctx| {
-            ctx.set_slave(SLAVE_1);
+    let serial_config = SerialConfig {
+        path: "/dev/ttyUSB0".into(),
+        settings: SerialPortSettings {
+            baud_rate: 19200,
+            ..Default::default()
+        },
+        handle: core.handle(),
+    };
+    println!("Configuration: {:?}", serial_config);
+
+    // A shared, reconnectable context is not actually needed in this
+    // simple example. Nevertheless we use it here to demonstrate how
+    // it works.
+    let shared_context = Rc::new(RefCell::new(SharedContext::new(
+        None, // no initial context, i.e. not connected
+        Box::new(serial_config),
+    )));
+
+    // Reconnect for connecting an initial context
+    let task = reconnect_shared_context(&shared_context)
+        .map(move |()| {
+            assert!(shared_context.borrow().is_connected());
+            println!("Connected");
+            shared_context
+        })
+        .and_then(move |shared_context| {
             println!("Reading a sensor value from {:?}", SLAVE_1);
-            ctx.read_holding_registers(0x082B, 2)
-                .and_then(|rsp| Ok((ctx, rsp)))
+            let context = shared_context.borrow().share_context().unwrap();
+            let mut context = context.borrow_mut();
+            context.set_slave(SLAVE_1);
+            context
+                .read_holding_registers(0x082B, 2)
+                .map(move |response| (shared_context, response))
         })
-        .and_then(move |(ctx, rsp)| {
-            println!("Sensor value for device {:?} is: {:?}", SLAVE_1, rsp);
-            Ok(ctx)
+        .map(move |(shared_context, response)| {
+            println!("Sensor value for device {:?} is: {:?}", SLAVE_1, response);
+            shared_context
         })
-        .and_then(|mut ctx| {
-            ctx.set_slave(SLAVE_2);
+        .and_then(move |shared_context| {
             println!("Reading a sensor value from {:?}", SLAVE_2);
-            ctx.read_holding_registers(0x082B, 2)
-                .and_then(|rsp| Ok((ctx, rsp)))
+            let context = shared_context.borrow().share_context().unwrap();
+            let mut context = context.borrow_mut();
+            context.set_slave(SLAVE_2);
+            context
+                .read_holding_registers(0x082B, 2)
+                .map(move |response| (shared_context, response))
         })
-        .and_then(move |(_, rsp)| {
-            println!("Sensor value for device {:?} is: {:?}", SLAVE_2, rsp);
-            Ok(())
+        .map(move |(_, response)| {
+            println!("Sensor value for device {:?} is: {:?}", SLAVE_2, response);
+            // Done
         });
 
     core.run(task).unwrap();
