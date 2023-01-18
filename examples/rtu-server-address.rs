416c96d48f9c7f719c47dc54240335b673d646a2
obayemi <obayemi@ikjpz.com>
2023-01-18 23:53:41 +0100

    add rtu-server-address example

---
+++ examples/rtu-server-address.rs
@@ -0,0 +1,64 @@
+// SPDX-FileCopyrightText: Copyright (c) 2017-2022 slowtec GmbH <post@slowtec.de>
+// SPDX-License-Identifier: MIT OR Apache-2.0
+
+//! RTU server example
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    use futures::future;
+    use std::{thread, time::Duration};
+
+    use tokio_modbus::prelude::*;
+    use tokio_modbus::server::{self, Service};
+
+    struct MbServer {
+        slave: Slave,
+    };
+
+    impl Service for MbServer {
+        type Request = SlaveRequest;
+        type Response = Option<Response>;
+        type Error = std::io::Error;
+        type Future = future::Ready<Result<Self::Response, Self::Error>>;
+
+        fn call(&self, req: Self::Request) -> Self::Future {
+            if req.slave != self.slave.into() {
+                return future::ready(Ok(None));
+            }
+            match req.request {
+                Request::ReadInputRegisters(_addr, cnt) => {
+                    let mut registers = vec![0; cnt.into()];
+                    registers[2] = 0x77;
+                    future::ready(Ok(Some(Response::ReadInputRegisters(registers))))
+                }
+                _ => unimplemented!(),
+            }
+        }
+    }
+
+    let slave = Slave(12);
+    let builder = tokio_serial::new("/dev/ttyS10", 19200);
+    let server_serial = tokio_serial::SerialStream::open(&builder).unwrap();
+
+    println!("Starting up server...");
+    let _server = thread::spawn(move || {
+        let rt = tokio::runtime::Runtime::new().unwrap();
+        let server = server::rtu::Server::new(server_serial);
+        let slave = slave.clone();
+        rt.block_on(async {
+            server.serve_forever(move || Ok(MbServer { slave })).await;
+        });
+    });
+
+    // Give the server some time for stating up
+    thread::sleep(Duration::from_secs(1));
+
+    println!("Connecting client...");
+    let client_serial = tokio_serial::SerialStream::open(&builder).unwrap();
+    let mut ctx = rtu::connect_slave(client_serial, slave).await?;
+    println!("Reading input registers...");
+    let rsp = ctx.read_input_registers(0x00, 7).await?;
+    println!("The result is '{:#x?}'", rsp); // The result is '[0x0,0x0,0x77,0x0,0x0,0x0,0x0,]'
+
+    Ok(())
+}
