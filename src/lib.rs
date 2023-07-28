37b53a83f12948c41cf2b7154e56e0d028793c73
Uwe Klotz <uwe.klotz@slowtec.de>
2023-07-29 00:23:49 +0200

    Update linting rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,25 @@
 // SPDX-FileCopyrightText: Copyright (c) 2017-2023 slowtec GmbH <post@slowtec.de>
 // SPDX-License-Identifier: MIT OR Apache-2.0
 
+#![warn(rustdoc::broken_intra_doc_links)]
+#![doc = include_str!("../README.md")]
 #![warn(rust_2018_idioms)]
 #![warn(rust_2021_compatibility)]
 #![warn(missing_debug_implementations)]
+//#![warn(missing_docs)] // TODO
 #![warn(unreachable_pub)]
-#![cfg_attr(not(test), warn(unsafe_code))]
+#![warn(unsafe_code)]
+#![warn(rustdoc::broken_intra_doc_links)]
 #![warn(clippy::pedantic)]
+// Additional restrictions
 #![warn(clippy::clone_on_ref_ptr)]
+#![warn(clippy::self_named_module_files)]
+// Exceptions
 #![allow(clippy::enum_glob_use)]
 #![allow(clippy::similar_names)]
 #![allow(clippy::module_name_repetitions)]
 #![allow(clippy::wildcard_imports)] // TODO
 #![allow(clippy::missing_errors_doc)] // TODO
-#![warn(rustdoc::broken_intra_doc_links)]
-#![doc = include_str!("../README.md")]
 
 /// Re-export the `bytes` crate
 ///
