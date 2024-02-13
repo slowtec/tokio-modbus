dec1a97bf26a511fa55c36eff8ffe399af65ef47
Benjamin Peter <bpeter@tuta.com>
2024-02-13 11:13:32 +0100

    refacto(examples): updated `rtu-server` with new `Client` API

--- examples/rtu-server.rs
+++ examples/rtu-server.rs
@@ -20,6 +20,9 @@ impl tokio_modbus::server::Service for Service {
                 registers[2] = 0x77;
                 future::ready(Ok(Response::ReadInputRegisters(registers)))
             }
+            Request::ReadHoldingRegisters(_, _) => {
+                future::ready(Err(Exception::IllegalDataAddress))
+            }
             _ => unimplemented!(),
         }
     }
@@ -45,12 +48,22 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Give the server some time for stating up
     thread::sleep(Duration::from_secs(1));
 
-    println!("Connecting client...");
+    println!("CLIENT: Connecting client...");
     let client_serial = tokio_serial::SerialStream::open(&builder).unwrap();
     let mut ctx = rtu::attach(client_serial);
-    println!("Reading input registers...");
+    println!("CLIENT: Reading input registers...");
     let rsp = ctx.read_input_registers(0x00, 7).await?;
-    println!("The result is '{rsp:#x?}'"); // The result is '[0x0,0x0,0x77,0x0,0x0,0x0,0x0,]'
+    println!("CLIENT: The result is '{rsp:#x?}'");
+    assert_eq!(rsp, Ok(vec![0x0, 0x0, 0x77, 0x0, 0x0, 0x0, 0x0]));
 
+    // Now we try to read with an invalid register address.
+    // This should return a Modbus exception response with the code
+    // IllegalDataAddress.
+    println!("CLIENT: Reading nonexisting holding register address... (should return IllegalDataAddress)");
+    let response = ctx.read_holding_registers(0x100, 1).await.unwrap();
+    println!("CLIENT: The result is '{response:?}'");
+    assert_eq!(response, Err(Exception::IllegalDataAddress));
+
+    println!("CLIENT: Done.");
     Ok(())
 }
